b73e21a6f6b396d44678a6b8f8fa0a13dbac27c4
Adarsh.Konchady <adarsh.konchady@target.com>
2019-01-07 00:40:50 -0600

    Initial commit
    Author: Adarsh Konchady <akonchady@gmail.com>
    Date: Mon Jan 7 00:40:50 2019 -0600

---
+++ README.md
@@ -0,0 +1,16 @@
+This package can be used to disable all existing errors and warnings in your project by adding a comment to disable them.
+
+- Motivation:
+  You might have an existing project which has a lot of errors or/and warnings but you do not want these errors, warnings to increase further. Use this package to comment all existing occurences and put in ESLint rule henceforth to detect all bad code.
+
+- Usage:
+
+```
+disable-eslint-issues-cli <directory_path>
+```
+
+Example:
+
+```
+disable-eslint-issues-cli src/**/*.js
+```
--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,94 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
 
-shell.exec("echo shell.exec works well");
+const { CLIEngine } = require("eslint");
+const { appendFile, readFile, writeFile } = require("fs");
+
+shell.exec("echo Processing your files ...");
+
+const log = err => {
+  if (err) {
+    throw err;
+  }
+};
+
+const getEslintWarningAndErrorReport = () => {
+  const args = process.argv.slice(2);
+  const cli = new CLIEngine({
+    // configFile: eslintConfigPath,
+    envs: ["browser", "mocha"]
+    //   useEslintrc: false,
+  });
+
+  shell.exec("echo Getting a report for bad rules...");
+  const report = cli.executeOnFiles(args);
+
+  return report;
+};
+
+const writeModifiedRules = ruleIdsArr => {
+  const uniqueRuleIds = new Set();
+  ruleIdsArr.forEach(ruleSet => {
+    Array.from(ruleSet).forEach(ruleSetElement => {
+      uniqueRuleIds.add(ruleSetElement);
+    });
+  });
+  const ruleSetText = Array.from(uniqueRuleIds).join("\n");
+  appendFile("./modifiedRuleIds.txt", ruleSetText, log);
+};
+
+const disableESLintIssues = results => {
+  const ruleIds = new Set();
+  const readFilePromises = results.map(result => {
+    const { messages, filePath } = result;
+    const lineMeta = [];
+
+    // Parse failing rules
+    messages.forEach(message => {
+      const { line, ruleId } = message;
+
+      // TODO: Handle multiple failing rules per line
+      lineMeta.push({
+        ruleId,
+        lineNumber: line
+      });
+    });
+
+    return new Promise(resolve => {
+      readFile(filePath, (err, data) => {
+        log(err);
+
+        const fileContentsArr = data.toString().split("\n");
+        let linePadding = 0;
+        // `lineMeta` has the line numbers to disable eslint rules.
+        lineMeta.forEach(({ ruleId, lineNumber }) => {
+          // TODO: Handle JSX comments
+          fileContentsArr.splice(
+            lineNumber - 1 + linePadding,
+            0,
+            `// eslint-disable-next-line ${ruleId}`
+          );
+          linePadding++; // increment linePadding since every insertion increases lineNumber by 1
+          ruleIds.add(ruleId);
+        });
+        const text = fileContentsArr.join("\n");
+        writeFile(filePath, text, log);
+        resolve(ruleIds);
+      });
+    });
+  });
+  return readFilePromises;
+};
+
+const transformEslintWarnings = () => {
+  const { results: resultsArr } = getEslintWarningAndErrorReport();
+  shell.exec("echo Disabling ESLint issues...");
+  const promises = disableESLintIssues(resultsArr);
+  shell.exec("echo Writing modified rules to file...");
+  Promise.all(promises).then(ruleIdsArr => {
+    writeModifiedRules(ruleIdsArr);
+  });
+  shell.exec("echo Done");
+};
+
+transformEslintWarnings();
